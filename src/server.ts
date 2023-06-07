1a6c96012273ce878b2f634ae9da0009e3b326c1
Ana Claudia <anacnogueira@gmail.com>
2023-06-07 20:00:06 -0300

    Utilizando cookies no fastify

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import fastify from "fastify";
+import cookie from "@fastify/cookie";
 import { env } from "./env";
 import { transactiosRoutes } from "./routes/transactions";
 
 const app = fastify();
 
+app.register(cookie);
 app.register(transactiosRoutes, {
   prefix: "transactions",
 });
