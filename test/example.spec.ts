1ceb96c56525cb291e0f32356db885fe85e7f44c
Ana Claudia <anacnogueira@gmail.com>
2023-06-13 11:14:34 -0300

    Instalando vitest

---
+++ test/example.spec.ts
@@ -0,0 +1,6 @@
+import { expect, test } from "vitest";
+
+test("O usuario consegue criar uma nova transac]ção", () => {
+  const responseStatusCode = 500;
+  expect(responseStatusCode).toEqual(201);
+});
