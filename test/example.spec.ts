1dbfe9329b5e1bf664853a17d2cc1585db1208a4
Ana Claudia <anacnogueira@gmail.com>
2023-06-13 18:57:49 -0300

    testando a criação de uma transação

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,6 +1,22 @@
-import { expect, test } from "vitest";
+import { expect, test, beforeAll, afterAll } from "vitest";
+import request from "supertest";
+import { app } from "../src/app";
 
-test("O usuario consegue criar uma nova transac]ção", () => {
-  const responseStatusCode = 500;
-  expect(responseStatusCode).toEqual(201);
+beforeAll(async () => {
+  await app.ready();
+});
+
+afterAll(async () => {
+  await app.close();
+});
+
+test("O usuario consegue criar uma nova transac]ção", async () => {
+  const response = await request(app.server)
+    .post("/transactions")
+    .send({
+      title: "New Transacation",
+      amount: 5000,
+      type: "credit",
+    })
+    .expect(201);
 });
