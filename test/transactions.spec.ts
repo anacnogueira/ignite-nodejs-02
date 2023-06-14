8eb38ff750a0cae28140d2e8b9bd39af10b8e3ba
Ana Claudia <anacnogueira@gmail.com>
2023-06-14 16:30:20 -0300

    Configurando banco de dados de teste

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,12 @@
-import { test, beforeAll, afterAll, describe, expect } from "vitest";
+import {
+  test,
+  beforeAll,
+  afterAll,
+  describe,
+  expect,
+  beforeEach,
+} from "vitest";
+import { execSync } from "node:child_process";
 import request from "supertest";
 import { app } from "../src/app";
 
@@ -9,6 +17,11 @@ describe("Transaction Routes", () => {
 
   afterAll(async () => {
     await app.close();
+  });
+
+  beforeEach(() => {
+    execSync("npm run knex migrate:rollback --all");
+    execSync("npm run knex migrate:latest");
   });
 
   test("It should be able to create a new transaction", async () => {
